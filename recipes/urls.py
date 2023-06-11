6e56fea924c29ce6d76145a2d860b768a700321f
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-06-10 22:43:46 -0300

    feat: url & templates  changes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipes, name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from recipes.models import *
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(9)]})
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
 
 def recipes(request, id: int):
     return render(request, 'recipes/pages/recipe_view.html', context={'recipe': make_recipe(), 'is_detail_page': True})
