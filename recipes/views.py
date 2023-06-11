0e28ef34c5e3331d47589290ac02186872b265d8
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-06-10 23:23:44 -0300

    feat: views change

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import *
 
@@ -9,8 +8,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+    return render(request, 'recipes/pages/category.html', context={'recipes': recipes, 'title': f'{recipes[0].category.name} - Category'})
 
 def recipes(request, id: int):
     return render(request, 'recipes/pages/recipe_view.html', context={'recipe': make_recipe(), 'is_detail_page': True})
